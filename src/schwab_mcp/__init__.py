7afc3e4d5277db329682f5fc716e0b068d62a606
Jason Kölker <jason@koelker.net>
2025-10-13 16:25:34 +0000

    fix(cli): defer CLI import

--- src/schwab_mcp/__init__.py
+++ src/schwab_mcp/__init__.py
@@ -1,5 +1,13 @@
 #
 
-from schwab_mcp.cli import main
+from typing import Any
+
+
+def main(*args: Any, **kwargs: Any):
+    """Entry point proxy that defers CLI imports until invocation."""
+    from schwab_mcp.cli import main as cli_main
+
+    return cli_main(*args, **kwargs)
+
 
 __all__ = ["main"]
